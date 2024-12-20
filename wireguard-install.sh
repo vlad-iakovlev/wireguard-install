9589133c36eaa873f5d5d6fbf3d92c1e365bb15b
Vladislav Iakovlev <vlad309523@gmail.com>
2024-12-20 14:16:34 +0000

    feat: disable client name length check

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -293,9 +293,9 @@ function newClient() {
 	echo ""
 	echo "Client configuration"
 	echo ""
-	echo "The client name must consist of alphanumeric character(s). It may also include underscores or dashes and can't exceed 15 chars."
+	echo "The client name must consist of alphanumeric character(s). It may also include underscores or dashes."
 
-	until [[ ${CLIENT_NAME} =~ ^[a-zA-Z0-9_-]+$ && ${CLIENT_EXISTS} == '0' && ${#CLIENT_NAME} -lt 16 ]]; do
+	until [[ ${CLIENT_NAME} =~ ^[a-zA-Z0-9_-]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Client name: " -e CLIENT_NAME
 		CLIENT_EXISTS=$(grep -c -E "^### Client ${CLIENT_NAME}\$" "/etc/wireguard/${SERVER_WG_NIC}.conf")
 
